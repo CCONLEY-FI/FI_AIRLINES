625916826db296c28aa1244f42bb4bb9e297d2bb
Nick Drummond <nckdrmmnd@gmail.com>
2024-03-07 16:27:53 -0700

    deleted dupes

--- react-login/src/Components/LoginForm/LoginForm.jsx
+++ react-login/src/Components/LoginForm/LoginForm.jsx
@@ -5,14 +5,6 @@ import { RiLockPasswordFill } from "react-icons/ri";
 import { useNavigate } from 'react-router-dom';
 
 
-import React from 'react';
-import "./LoginForm.css";
-import { FaUserAstronaut } from "react-icons/fa";
-import { RiLockPasswordFill } from "react-icons/ri";
-import { useNavigate } from 'react-router-dom';
-
-
-
 const LoginForm = () => {
     const navigate = useNavigate()
     const handleRegister = () => {
@@ -29,11 +21,7 @@ const LoginForm = () => {
                     <input type='text' placeholder='Username'/>
                     <FaUserAstronaut className='icon' />
                 </div>
-                <div className='input-field'>
-                    <input type='password' placeholder='Password'/>
-                    <RiLockPasswordFill className='icon' />
-
-                </div>
+                
                 <div className='input-field'>
                     <input type='submit' value='Login' onClick={handleSignIn}/>
                 </div>
@@ -42,14 +30,11 @@ const LoginForm = () => {
                     <a href='#'>Forget Password</a>
                     <input type='submit' value='Login' onClick={handleSignIn}/>
                 </div>
-                <div className='remember-me-forget'>
-                    <label><input type="checkbox"/>Remember Me</label>
-                    <a href='#'>Forget Password</a>
-                </div>
+                
                 {/* <button type='submit'>Login</button> */}
                 <div className="register"><p>Don't have an account? <a onClick={handleRegister}>Register</a></p></div>
-                {/* <button type='submit'>Login</button> */}
-                <div className="register"><p>Don't have an account? <a onClick={handleRegister}>Register</a></p></div>
+               
+                
             </form>
             
 
