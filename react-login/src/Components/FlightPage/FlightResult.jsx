682cf698d329c247171cfac4de042fe75111e6a2
Kyle Geary <gearykye34@gmail.com>
2024-03-07 14:06:55 -0700

    updated flights page

--- react-login/src/Components/FlightPage/FlightResult.jsx
+++ react-login/src/Components/FlightPage/FlightResult.jsx
@@ -25,15 +25,27 @@ const FlightResult = () => {
       <form>
           <div className="input-field">
               <label htmlFor="origin">From</label>
-              <input type="text" id="origin" name="origin" placeholder="Origin" required />
+              <input type="text" id="origin" name="origin" placeholder="Origin"  />
           </div>
           <div className="input-field">
               <label htmlFor="destination">To</label>
-              <input type="text" id="destination" name="destination" placeholder="Destination" required />
+              <input type="text" id="destination" name="destination" placeholder="Destination"  />
           </div>
           <div className="input-field">
               <label htmlFor="date">Departure Date</label>
-              <input type="date" id="date" name="date" required />
+              <input type="date" id="date" name="date"  />
+          </div>
+          <div className="input-field">
+              <label htmlFor="time">Departure Time</label>
+              <input type="text" id="departure-time" name="departure-time" placeholder='Departure Time'  />
+          </div>
+          <div className="input-field">
+              <label htmlFor="time">Arival Time</label>
+              <input type="text" id="arival-time" name="arival-time" placeholder='Arival Time'  />
+          </div>
+          <div className="input-field">
+              <label htmlFor="flight-number">Flight Number</label>
+              <input type="text" id="flight-number" name="flight-number" placeholder='Flight Number'  />
           </div>
           <button type="submit" className="submit-btn">Save Flight</button>
       </form>
