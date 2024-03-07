351ea01c546988154afe15f13f8c8e439d6f147b
Kyle Geary <gearykye34@gmail.com>
2024-03-07 09:19:53 -0700

    added template for our filtered flights

--- react-login/src/Components/Homepage/Homepage.jsx
+++ react-login/src/Components/Homepage/Homepage.jsx
@@ -11,21 +11,13 @@ const Homepage = () => {
         <form action=''>
             <h1>Browse Flights</h1>
             <div className='input-field'>
-                <input type='text' placeholder='Origin'/>
+                <input type='text' placeholder='Origin' required/>
             </div>
             <div className='input-field'>
-                <input type='text' placeholder='Destination'/>
+                <input type='text' placeholder='Destination' required/>
             </div>
             <div className='input-field'>
-                <input type='date' placeholder='Date'/>
-            </div>
-            <div className='input-field'>
-                <select>
-                    <option value="Economy">Economy</option>
-                    <option value="Business">Business</option>
-                    <option value="First Class">First Class</option>
-                </select>
-                {/* <input type='dropdown' placeholder='Verify Password'/> */}
+                <input type='date' placeholder='Date' required/>
             </div>
             <button type='submit' onClick={handleSearchFlights}>Search Flights</button>
         </form>
